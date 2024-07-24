c756e179575b8f3b85d49bf39bea3dc38ee3544d
avhiejeet <abhijitrajbanshi43@gmail.com>
2024-07-24 23:41:21 +0545

    form view update

--- models/models.py
+++ models/models.py
@@ -10,10 +10,21 @@ class RepairRequest(models.Model):
     _description = 'Repair Request'
 
     repair_request_name = fields.Char(string="Repair Request Name", required=True)
+    repair_reference = fields.Char(string="Repair Reference", copy=False, readonly=True,
+                                          default='New')
     description = fields.Text(string="Description")
     partner_id = fields.Many2one('res.partner', string='Customer', required=True)
-    product_id = fields.Many2one('product.product', string='Product', required=True)
+    product_id = fields.Many2one('product.product', string='Product to Repair', required=True)
     repair_image = fields.Binary(string="Repair Image", attachment=True)
+    under_warranty = fields.Boolean(string="Under Warranty")
+    scheduled_date = fields.Datetime(string="Scheduled Date")
+    responsible_user_id = fields.Many2one('res.users', string="Responsible")
+    # tag_ids = fields.Many2many('res.tag', string="Tags")
+    component_status = fields.Selection(
+        [('green', 'Available'), ('red', 'Unavailable')],
+        string="Component Status",
+        default='red'
+    )
     status = fields.Selection(
         [('new', 'New'),
          ('quotation', 'Quotation'),
@@ -28,6 +39,11 @@ class RepairRequest(models.Model):
     quotation_id = fields.Many2one('sale.order', string="Quotation")
     sale_order_id = fields.Many2one('sale.order', string="Sales Order")
 
+    @api.model
+    def create(self, vals):
+        if vals.get('repair_reference', 'New') == 'New':
+            vals['repair_reference'] = self.env['ir.sequence'].next_by_code('repair_request.repair_request') or 'New'
+        return super(RepairRequest, self).create(vals)
     def send_for_review_button_method(self):
         self.status = 'client_review'
     def cancel_button_method(self):
@@ -60,7 +76,6 @@ class RepairRequest(models.Model):
             self.quotation_id.action_confirm()
             self.sale_order_id = self.quotation_id.id
             self.status = 'accepted'
-
 
 
 
