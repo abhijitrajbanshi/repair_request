fd496df86da7aceab9579585cb03ff4b705ca071
Pradeep Pandey <pradeep@dfwitpartner.com>
2024-08-01 16:21:51 +0545

    client-review (#16)

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -129,7 +129,7 @@ class CustomerPortalHome(CustomerPortal):
         repair_request = request.env['repair_request.repair_request'].sudo().browse(repair_id)
         if not repair_request.exists() or repair_request.partner_id.id != request.env.user.partner_id.id:
             return request.redirect('/my/repair_requests')
-        if repair_request.status not in ['new', 'quotation']:
+        if repair_request.status not in ['new', 'quotation', 'client_review']:
             return request.redirect('/my/repair_requests')
 
         images = []
@@ -153,7 +153,7 @@ class CustomerPortalHome(CustomerPortal):
         repair_request = request.env['repair_request.repair_request'].sudo().browse(repair_id)
         if not repair_request.exists() or repair_request.partner_id.id != request.env.user.partner_id.id:
             return request.redirect('/my/repair_requests')
-        if repair_request.status not in ['new', 'quotation']:
+        if repair_request.status not in ['new', 'quotation', 'client_review']:
             return request.redirect('/my/repair_requests')
 
         # Handle datetime conversion
